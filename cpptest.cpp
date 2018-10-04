8473ac1f239c4844699856b36c1859e8302e382e
SigurdSor <sigurdsor@gmail.com>
2018-10-04 16:44:03 +0200

    Testet mere c++

--- cpptest.cpp
+++ cpptest.cpp
@@ -1,6 +1,9 @@
 
 #include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
+
 
 void say_hello() {
 	cout << "Hello!" << endl;
@@ -12,10 +15,38 @@ void reduction_by_halves(int n){
 		n = n / 2;
 	}
 }
+
+double stirling(int x){
+    return x*log(x)-x;
+}
+
+int triangle(int n) {
+    int triangle_sum = 0;
+    int i = 0;
+    while (i < n+1) {
+        triangle_sum += i;
+        i += 1;
+    }
+    return triangle_sum;
+}
+
+
+vector<double> linspace(double a, double b, int n) {
+    vetctor
+}
+
+
 int main()
 {
-	int n = 100;
+	int n = 1000;
+    vector<int> testvector{2, 5, 10, 50, 100, 1000};
 	say_hello();
 	reduction_by_halves(n);
+    for (int i=0; i<6; i++) {
+        double stir = stirling(testvector[i]);
+        double real = lgamma(testvector[i]+1);
+        cout << stir-real << endl;
+    }
+    cout << triangle(n) << endl;
 	return 0;
 }
